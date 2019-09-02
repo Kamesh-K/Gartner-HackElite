371f09c4cfba5f57192a8917eb71c04cbaae2017
Kamesh K <mm16b107@smail.iitm.ac.in>
2019-09-02 11:32:07 +0530

    Adding the final set of files

---
+++ JN-XGBoost/Data_Preprocessing.py
@@ -0,0 +1,29 @@
+Act1=X['Activity 1 Time Period 11']+X['Activity 1 Time Period 10']+X['Activity 1 Time Period 9']+X['Activity 1 Time Period 8']+X['Activity 1 Time Period 7']+X['Activity 1 Time Period 6']+X['Activity 1 Time Period 5']+X['Activity 1 Time Period 4']+X['Activity 1 Time Period 3']+X['Activity 1 Time Period 2']+X['Activity 1 Time Period 1']+X['Activity 1 Time Period 0']
+Act2=X['Activity 2 Time Period 11']+X['Activity 2 Time Period 10']+X['Activity 2 Time Period 9']+X['Activity 2 Time Period 8']+X['Activity 2 Time Period 7']+X['Activity 2 Time Period 6']+X['Activity 2 Time Period 5']+X['Activity 2 Time Period 4']+X['Activity 2 Time Period 3']+X['Activity 2 Time Period 2']+X['Activity 2 Time Period 1']+X['Activity 2 Time Period 0']
+Act3=X['Activity 3 Time Period 11']+X['Activity 3 Time Period 10']+X['Activity 3 Time Period 9']+X['Activity 3 Time Period 8']+X['Activity 3 Time Period 7']+X['Activity 3 Time Period 6']+X['Activity 3 Time Period 5']+X['Activity 3 Time Period 4']+X['Activity 3 Time Period 3']+X['Activity 3 Time Period 2']+X['Activity 3 Time Period 1']+X['Activity 3 Time Period 0']
+Act4=X['Activity 4 Time Period 11']+X['Activity 4 Time Period 10']+X['Activity 4 Time Period 9']+X['Activity 4 Time Period 8']+X['Activity 4 Time Period 7']+X['Activity 4 Time Period 6']+X['Activity 4 Time Period 5']+X['Activity 4 Time Period 4']+X['Activity 4 Time Period 3']+X['Activity 4 Time Period 2']+X['Activity 4 Time Period 1']+X['Activity 4 Time Period 0']
+Act5=X['Activity 5 Time Period  11']+X['Activity 5 Time Period  10']+X['Activity 5 Time Period  9']+X['Activity 5 Time Period  8']+X['Activity 5 Time Period  7']+X['Activity 5 Time Period  6']+X['Activity 5 Time Period  5']+X['Activity 5 Time Period  4']+X['Activity 5 Time Period  3']+X['Activity 5 Time Period  2']+X['Activity 5 Time Period  1']+X['Activity 5 Time Period  0']
+Act6=X['Activity 6 Time Period  11']+X['Activity 6 Time Period  10']+X['Activity 6 Time Period  9']+X['Activity 6 Time Period  8']+X['Activity 6 Time Period  7']+X['Activity 6 Time Period  6']+X['Activity 6 Time Period  5']+X['Activity 6 Time Period  4']+X['Activity 6 Time Period  3']+X['Activity 6 Time Period  2']+X['Activity 6 Time Period  1']+X['Activity 6 Time Period  0']
+Act7=X['Activity 7 Time Period  11']+X['Activity 7 Time Period  10']+X['Activity 7 Time Period  9']+X['Activity 7 Time Period  8']+X['Activity 7 Time Period  7']+X['Activity 7 Time Period  6']+X['Activity 7 Time Period  5']+X['Activity 7 Time Period  4']+X['Activity 7 Time Period  3']+X['Activity 7 Time Period  2']+X['Activity 7 Time Period  1']+X['Activity 7 Time Period  0']
+Act8=X['Activity 8 Time Period 11']+X['Activity 8 Time Period 10']+X['Activity 8 Time Period 9']+X['Activity 8 Time Period 8']+X['Activity 8 Time Period 7']+X['Activity 8 Time Period 6']+X['Activity 8 Time Period 5']+X['Activity 8 Time Period 4']+X['Activity 8 Time Period 3']+X['Activity 8 Time Period 2']+X['Activity 8 Time Period 1']+X['Activity 8 Time Period 0']
+scale(Act1)
+scale(Act1)
+scale(Act2)
+scale(Act3)
+scale(Act4)
+scale(Act5)
+scale(Act6)
+scale(Act7)
+scale(Act8)
+X['Act1']=Act1
+X['Act2']=Act2
+X['Act3']=Act3
+X['Act4']=Act4
+X['Act5']=Act5
+X['Act6']=Act6
+X['Act7']=Act7
+X['Act8']=Act8
+'Activity 5 Time Period  11','Activity 5 Time Period  10','Activity 5 Time Period  9','Activity 5 Time Period  8','Activity 5 Time Period  7','Activity 5 Time Period  6','Activity 5 Time Period  5','Activity 5 Time Period  4','Activity 5 Time Period  3','Activity 5 Time Period  2','Activity 5 Time Period  1','Activity 5 Time Period  0'
+'Activity 6 Time Period  11','Activity 6 Time Period  10','Activity 6 Time Period  9','Activity 6 Time Period  8','Activity 6 Time Period  7','Activity 6 Time Period  6','Activity 6 Time Period  5','Activity 6 Time Period  4','Activity 6 Time Period  3','Activity 6 Time Period  2','Activity 6 Time Period  1','Activity 6 Time Period  0'
+'Activity 7 Time Period  11','Activity 7 Time Period  10','Activity 7 Time Period  9','Activity 7 Time Period  8','Activity 7 Time Period  7','Activity 7 Time Period  6','Activity 7 Time Period  5','Activity 7 Time Period  4','Activity 7 Time Period  3','Activity 7 Time Period  2','Activity 7 Time Period  1','Activity 7 Time Period  0'
+'Activity 8 Time Period 11','Activity 8 Time Period 10','Activity 8 Time Period 9','Activity 8 Time Period 8','Activity 8 Time Period 7','Activity 8 Time Period 6','Activity 8 Time Period 5','Activity 8 Time Period 4','Activity 8 Time Period 3','Activity 8 Time Period 2','Activity 8 Time Period 1','Activity 8 Time Period 0'
